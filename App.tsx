3d7be6c8a45e551f7d3eb53b03f07ee8580e1af7
YJouhar47 <youssef.jouhar1998@hotmail.com>
2022-11-30 00:29:29 +0100

    data uit api halen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,10 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SecondScreen from "./components/SecondScreen";
 import HomeScreen from "./components/HomeScreen";
-import ThirdScreen from "./components/ThirdScreen";
-
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Second" component={SecondScreen}/>
-        <Stack.Screen name="Third" component={ThirdScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <HomeScreen></HomeScreen>
+  )
 }
 
 export default App;
--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,23 +1,37 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
+import { Book } from "../books"
 
 const HomeScreen = () => {
-    const navigation : any = useNavigation();
-    return (
-      
-      <View style={{borderColor:"black"}}>
-        <Text style={{fontSize:25, textAlign:"center"}}>Welkom bij de mooiste applicatie</Text>
-        <View style={{borderColor:"black"}}>
-        
-        <Text onPress={() => navigation.navigate("Detail")} style={{fontSize:12, textAlign:"center", textDecorationLine:"underline"}}>Klik hier om verder te gaan</Text>
-        <Text onPress={() => navigation.navigate("Third")} style={{fontSize:12, textAlign:"center"}}>Klik hier om naar ergens anders te gaan</Text>
+  const [books,setBooks] = useState<Book[]>([])
+  useEffect(() => {
+    const apiCall = async () => {
+      const data = await fetch("https://api.itbook.store/1.0/search/mongodb%22");
+      const res = await data.json();
+      const x : Book[] = await res.books;
+      setBooks(x);
+    }
+    apiCall();
+  },[])
+  return (
+    <View style={styles.container}>
+      {books.map(book => {
+        return <View key={book.title}>
+        <Text>{book.title}</Text>
+        <Image style={{width:40, height:50}} source={{uri:book.image}}/>
         </View>
-      </View>
-    );
-  }
-
-  export default HomeScreen
-  
-  
+      })}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default HomeScreen;
