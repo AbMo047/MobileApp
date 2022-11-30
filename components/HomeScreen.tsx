ca127679bd4e3a5b5e9339345141df1c09af1cf6
YJouhar47 <youssef.jouhar1998@hotmail.com>
2022-11-30 13:00:44 +0100

    textinput search with api query search

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View,TextInput, Alert, Button } from "react-native";
 import Constants from "expo-constants";
 import { Book } from "../books"
 
 const HomeScreen = () => {
   const [books,setBooks] = useState<Book[]>([])
-  useEffect(() => {
-    const apiCall = async () => {
-      const data = await fetch("https://api.itbook.store/1.0/search/mongodb%22");
-      const res = await data.json();
-      const x : Book[] = await res.books;
-      setBooks(x);
-    }
-    apiCall();
-  },[])
+  const apiCall = async (query:string) => {
+    const data = await fetch(`https://api.itbook.store/1.0/search/${query}`);
+    const res = await data.json();
+    const x : Book[] = await res.books;
+    setBooks(x);
+  }
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      {books.map(book => {
-        return <View key={book.title}>
-        <Text>{book.title}</Text>
-        <Image style={{width:40, height:50}} source={{uri:book.image}}/>
-        </View>
-      })}
+      <TextInput style={{height:40,width:250, borderColor:"gray", borderWidth:1}}
+      onChangeText={text => setText(text)}
+      value={text}/>
+      <Button title="Show Value" onPress={() => { apiCall(text)}}/>
+      {
+        books.map(book=> {
+          return <View key={book.title}>
+            <Text>{book.title}</Text>
+            <Image style={{width:40, height:50}} source={{uri:book.image}}/>
+            </View>
+        })
+      }
+      
     </View>
-  );
+  )
 }
 const styles = StyleSheet.create({
   container: {
